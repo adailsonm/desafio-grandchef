c4e61f7a7af4fa0a73c404e82d53859b5333c9be
Adailson Moreira <adailsonmoreira@MacBook-Pro-de-Adailson.local>
2021-04-29 15:06:26 -0300

    Edit migration pessoas

---
+++ src/app/database/migrations/2014_10_12_000000_create_pessoas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePessoasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pessoas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->dateTime('data_nascimento');
+            $table->enum('genero', ['masculino', 'feminino'])->default(null);
+            $table->string('email')->unique();
+            $table->string('senha');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pessoas');
+    }
+}
